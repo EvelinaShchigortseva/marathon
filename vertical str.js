b8e57272a76a3124005a0b6217bd440e0587523d
Evelina Shchigortseva <shchigortseva.evelina@gmail.com>
2021-11-15 16:20:19 +1000

    after review

--- vertical str.js
+++ vertical str.js
@@ -1,27 +1,29 @@
 function showVerticalMessage(str) {
-    let newstr = '';
- 
-    function truncate(str) {
-      return (str.length > 10) ? str = str.slice(0, 10) : str;
-    }
-  
-    function toUpperCase(str) {
-      return (str[0] = "м") ? str[0].toUpperCase() + str.slice(1) : str;
-    }
-  
-    function vertical(str) {
-      for (let i = 0; i < str.length; i++) {
-        newstr += str[i] + "\n";
-      }
-      return newstr;
-    }
-  
-    let result = truncate(str);
-    result = toUpperCase(result)
-    result = vertical(result)
-    return result;
-  
+  let result = "";
+
+  function truncate(str) {
+    return str.length > 10 ? (str = str.slice(0, 10)) : str;
   }
-  
-  console.log(showVerticalMessage("марафончики")); 
-  
+
+  function toUpperCaseFirstSymbol(str) {
+    return (str[0] = "м") ? str[0].toUpperCase() + str.slice(1) : str;
+  }
+
+  function newLine(str) {
+    for (let i = 0; i < str.length; i++) {
+      result += str[i] + "\n";
+    }
+    return result;
+  }
+
+  return newLine(toUpperCaseFirstSymbol(truncate(str)));
+}
+
+console.log(showVerticalMessage("марафончики"));
+
+// const pipe =
+//   (...fns) =>
+//   (x) =>
+//     fns.reduce((v, f) => f(v), x);
+
+// return pipe(truncate, toUpperCaseFirstSymbol, newLine)(str);
