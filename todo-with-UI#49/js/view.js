eef03f77617b001c00e976944215fd1fcbee747b
Evelina Shchigortseva <shchigortseva.evelina@gmail.com>
2021-12-05 12:09:22 +1000

    delete console.log + add obj UI_elements

--- todo-with-UI#49/js/view.js
+++ todo-with-UI#49/js/view.js
@@ -1,40 +1,39 @@
 import {
   list,
-  statuses,
-  priorities,
-  id,
   addTask,
   deleteTask,
   changeTask,
 } from "./main.js";
 
-const forms = document.querySelectorAll(".form-input");
-const task_containers = document.querySelectorAll(".task_container");
+const UI_ELEMENTS = {
+    forms: document.querySelectorAll(".form-input"),
+    task_containers: document.querySelectorAll(".task_container"),
+}
 
-forms.forEach(function (form) {
+UI_ELEMENTS.forms.forEach(function (form) {
   form.addEventListener("submit", function (event) {
     event.preventDefault();
     
     let task_name = event.target.firstElementChild.value.trim();
     if(task_name){
-        let status = "To Do";
-        let priority = event.target.parentElement.id;
+        let task_status = "To Do";
+        let task_priority = event.target.parentElement.id;
 
-    let task_Element = addTask(task_name, status, priority);
+    let task_Element = addTask(task_name, task_status, task_priority);
 
     form.reset();
 
-    if (priority == "high") {
-      createBlockTask(task_Element.id, task_Element.name, task_containers[0]);
+    if (task_priority == "high") {
+      createBlockTask(task_Element.id, task_Element.name, UI_ELEMENTS.task_containers[0]);
     }
 
-    if (priority == "low") {
-      createBlockTask(task_Element.id, task_Element.name, task_containers[1]);
+    if (task_priority == "low") {
+      createBlockTask(task_Element.id, task_Element.name, UI_ELEMENTS.task_containers[1]);
     }
 
     console.log(list);
 }
-  });
+  }); 
 });
 
 function createBlockTask(id_task, task_name, task_container) {
@@ -69,16 +68,16 @@ function createBlockTask(id_task, task_name, task_container) {
 
 function deleteTaskElement(event) {
   let id_task = +event.target.previousElementSibling.previousElementSibling.id;
-  console.log(id_task);
+
   deleteTask(id_task);
   event.target.parentElement.remove();
-  console.log(list);
+  
 }
 
 function changeStatusElement(event) {
   let id_task = +event.target.previousElementSibling.id;
   let task_status;
-  console.log(id_task);
+  
 
   if (event.target.checked) {
     event.target.previousElementSibling.style.background = "#F4F4F4";
@@ -86,6 +85,7 @@ function changeStatusElement(event) {
   } else {
     event.target.previousElementSibling.style.background = "";
     task_status = "To Do";
+    
   }
   changeTask(id_task, task_status);
   console.log(list);
