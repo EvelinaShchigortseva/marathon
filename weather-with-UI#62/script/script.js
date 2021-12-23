d946cc6a3cfd9f21979228e0d7b591b0613252f1
Evelina Shchigortseva <shchigortseva.evelina@gmail.com>
2021-12-23 23:08:51 +1000

    added forecast

--- weather-with-UI#62/script/script.js
+++ weather-with-UI#62/script/script.js
@@ -1,6 +1,5 @@
-
 import {addCity, deleteCity, isCity} from "./list.js";
-import {getFavoriteCities,setFavoriteCity,deleteFavoriteCity} from "./storage.js";
+import {deleteFavoriteCity, getFavoriteCities, setFavoriteCity} from "./storage.js";
 
 const UI_ELEMENTS = {
     formSearch : document.querySelector('.form'),
@@ -16,6 +15,9 @@ const UI_ELEMENTS = {
     weather: document.querySelector('.weather'),
     sunrise: document.querySelector('.sunrise'),
     sunset: document.querySelector('.sunset'),
+    itemsList: document.querySelector('.weather_forecast_column')
+
+
 }
 let isFavorite
 
@@ -44,17 +46,19 @@ function colorFavorite(cityName){
 
 function initialState (){
     UI_ELEMENTS.degree.textContent = `...`
+
 }
 
 function catchError(city){
+
     switch (city.message){
-        case 'city not found' : UI_ELEMENTS.cityName.textContent = `City not found`; initialState(); break;
+        case 'city not found' : UI_ELEMENTS.cityText.textContent = `City not found`; initialState(); break;
         case 'Internal error' : alert('Internal error'); break;
     }
 }
 
 function setNow(city) {
-    console.log(city)
+
     let cityName = city.name
     UI_ELEMENTS.degree.textContent = `${parseInt(city.main.temp)}°C`
     UI_ELEMENTS.cityName[0].textContent = cityName
@@ -73,25 +77,106 @@ function setDetails(city) {
     let timeSunset = dateSunset.toTimeString().split(' ')[0].slice(0, -3);
 
 
-    let cityName = city.name;
-    UI_ELEMENTS.cityName[1].textContent = cityName;
+    UI_ELEMENTS.cityName[1].textContent = city.name;
     UI_ELEMENTS.temp.textContent = `${parseInt(city.main.temp)}°C`;
     UI_ELEMENTS.feels.textContent = `${parseInt(city.main.feels_like)}°C`;
     UI_ELEMENTS.weather.textContent = city.weather[0].main;
-    UI_ELEMENTS.sunrise.textContent = city.sys.sunrise;
-    UI_ELEMENTS.sunset.textContent = city.sys.sunset;
+    UI_ELEMENTS.sunrise.textContent = timeSunrise;
+    UI_ELEMENTS.sunset.textContent = timeSunset;
 }
 
 function setForecast(city) {
 
-    // const apiKey = 'f660a2fb1e4bad108d6160b7f58c555f';
+
+
     let cityName = city.name;
-    // UI_ELEMENTS.cityName[2].textContent = cityName;
-    // const url = `pro.openweathermap.org/data/2.5/forecast/hourly?q=${cityName}&appid=${apiKey}`
-    // fetch(url)
-    //     .then(response => response.json())
-    //     .then(console.log)
+    const serverUrl = 'http://api.openweathermap.org/data/2.5/forecast';
+    const apiKey = 'f660a2fb1e4bad108d6160b7f58c555f';
+    const url = `${serverUrl}?q=${cityName}&appid=${apiKey}&units=metric`
+
+    UI_ELEMENTS.cityName[2].textContent = cityName;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(cityInfo =>
+            getDate(cityInfo)
+        ).then(cityInfo => createItems(cityInfo))
+
+
+
 }
+
+
+
+function getDate(cityInfo) {
+    console.log(cityInfo.list)
+    const massItem = []
+    cityInfo.list.forEach((item, i) =>{
+        const arrayItem = {
+            time: new Date(cityInfo.list[i].dt_txt).toTimeString().split(' ')[0].slice(0, -3),
+            day: new Date(cityInfo.list[i].dt_txt).toDateString().split(' ').reverse().slice(1,3).join(' '),
+            temp: parseInt(cityInfo.list[i].main.temp),
+            feelsLike: parseInt(cityInfo.list[i].main.feels_like),
+            weather: cityInfo.list[i].weather[0].main,
+        }
+        massItem.push(arrayItem)
+    })
+   return massItem
+
+}
+
+function createItems (massItem){
+    massItem.forEach(arrayItem=>{
+        const itemsShell =  document.createElement("div");
+        itemsShell.classList.add('weather_forecast_item')
+
+        const itemDay =  document.createElement("div");
+        itemDay.classList.add('weather-forecast-item-date');
+        const itemTemp =  document.createElement("div");
+        itemTemp.classList.add('weather-forecast-item-temp')
+
+
+
+        itemsShell.append(itemDay,itemTemp)
+
+        const dayDate = document.createElement('div')
+        dayDate.classList.add('item-date-day')
+        dayDate.textContent = arrayItem.day
+        const timeDate = document.createElement('div')
+        timeDate.classList.add('item-date-time')
+        timeDate.textContent = arrayItem.time
+
+        itemDay.append(dayDate, timeDate)
+
+        const itemTempInfo = document.createElement('div')
+        itemTempInfo.classList.add('item-temp-info')
+        // переименовать на мэйн
+        const itemTempWeather = document.createElement('div')
+        itemTempWeather.classList.add('item-temp-weather')
+
+        itemTemp.append(itemTempInfo,itemTempWeather)
+        //
+        const temp = document.createElement('div')
+        temp.textContent = `Temperature: ${arrayItem.temp}`
+        const feel_like = document.createElement('div')
+        feel_like.textContent = `Feel like: ${arrayItem.feelsLike}`
+
+        itemTempInfo.append(temp,feel_like)
+
+        const weather = document.createElement('div')
+        weather.classList.add('item-weather')
+        weather.textContent = arrayItem.weather
+        const weather_img = document.createElement('div')
+        weather_img.classList.add('item-weather-img')
+
+        itemTempWeather.append(weather,weather_img)
+
+        UI_ELEMENTS.itemsList.append(itemsShell)
+    })
+
+}
+
+
 
 function deleteCityElement (event){
     let cityName = event.target.previousElementSibling.textContent
@@ -107,6 +192,7 @@ function deleteCityElement (event){
 }
 
 function showListCities(event){
+    UI_ELEMENTS.itemsList.textContent = ""
     let cityName = event.target.textContent
     let isFavorite = isCity(cityName)
     fetchQuery(cityName)
@@ -149,7 +235,10 @@ function createAddedLocationElements (cityName){
 
 UI_ELEMENTS.formSearch.addEventListener('submit', function (event){
     event.preventDefault()
+
+    UI_ELEMENTS.itemsList.textContent = ""
     const cityName = event.target.firstElementChild.value;
+
     fetchQuery(cityName)
     UI_ELEMENTS.formSearch.reset();
 })
@@ -158,7 +247,7 @@ UI_ELEMENTS.favoriteCity.addEventListener('click', function (event){
 
     const cityName = event.target.parentElement.firstElementChild.textContent
 
-    if(!isCity(cityName) && cityName != 'City not found' && cityName){
+    if(!isCity(cityName) && cityName !== 'City not found' && cityName){
         addCity(cityName)
         createAddedLocationElements(cityName)
         colorFavorite(cityName)
